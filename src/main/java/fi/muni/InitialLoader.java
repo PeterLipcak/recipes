bb4fa9d551b146c8e409234e44c89322fd5dc350
Peter Kovac <petookovac@gmail.com>
2017-12-07 23:52:38 +0100

    app modified for publishing to Google cloud

--- src/main/java/fi/muni/InitialLoader.java
+++ src/main/java/fi/muni/InitialLoader.java
@@ -25,38 +25,54 @@ public class InitialLoader implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
 
-        if(strings[0].split("=")[1].compareTo("yes") == 0){
-            RestTemplate restTemplate = new RestTemplate();
-
-            System.out.println("Downloading recipes...");
-            String json;
-
-            int i = 0;
-            while( i < 80)
-            {
-                System.out.println("Receiving json " + i);
-                try {
-                    json = restTemplate.getForObject("http://api.yummly.com/v1/api/recipes?_app_id=668c717d&_app_key=9123fc5b2c2a0742a8aa4ca1ef01bf35&maxResult=500&start=" + i * 500, String.class);
-                }catch (Exception e)
-                {
-                    continue;
-                }
-                System.out.println("Mapping recipes to object..." + i);
-                JsonNode arrNode = new ObjectMapper().readTree(json).get("matches");
-                List<Recipe> recipes = new ObjectMapper().enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL)
-                        .readValue(arrNode.toString(), new TypeReference<List<Recipe>>(){});
-                System.out.println("Jackson mapping was successful!");
-                System.out.println("Inserting to database...");
-                try {
-                    recipeDAO.save(recipes);
-                }
-                catch (Exception e)
-                {
-                    System.out.println("Could not persist downloaded data because of unsupported character occurence.");
-                }
-                i++;
-            }
-        }
+//        if(strings[0].split("=")[1].compareTo("yes") == 0){
+//            RestTemplate restTemplate = new RestTemplate();
+//
+//            System.out.println("Downloading recipes...");
+//            String json;
+//
+//            int i = 0;
+//            while( i < 80)
+//            {
+//                System.out.println("Receiving json " + i);
+//                try {
+//                    json = restTemplate.getForObject("http://api.yummly.com/v1/api/recipes?_app_id=668c717d&_app_key=9123fc5b2c2a0742a8aa4ca1ef01bf35&maxResult=500&start=" + i * 500, String.class);
+//                }catch (Exception e)
+//                {
+//                    continue;
+//                }
+//                System.out.println("Mapping recipes to object..." + i);
+//                JsonNode arrNode = new ObjectMapper().readTree(json).get("matches");
+//                List<Recipe> recipes = new ObjectMapper().enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL)
+//                        .readValue(arrNode.toString(), new TypeReference<List<Recipe>>(){});
+//                System.out.println("Jackson mapping was successful!");
+//                System.out.println("Inserting to database...");
+//                try {
+//                    recipeDAO.save(recipes);
+//                }
+//                catch (Exception e)
+//                {
+//                    System.out.println("Could not persist downloaded data because of unsupported character occurence.");
+//                }
+//                i++;
+//            }
+//        }
+//        List<Recipe> recipes = recipeDAO.findAll();
+//        List<Recipe> compareRecipes = recipeDAO.findAll();
+//
+//        recipes.stream().forEach(recipe -> {
+//            int i = 0;
+//            compareRecipes.stream().forEach(compRecipe -> {
+//                if(compRecipe != recipe)
+//                {
+//                    RecipeDifference recipeDifference = new RecipeDifference(recipe, compRecipe);
+//                    recipeDifferenceDAO.save(recipeDifference);
+//                }
+//            });
+//            i++
+//            System.out.println(recipe.getId());
+//        });
+        System.out.println("done");
         //System.out.println(recipeDAO.findOne(2));
     }
 }
